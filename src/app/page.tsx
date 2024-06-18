dc978fb1765fabf77f1fa60fe0be47deca76e49f
Tahjour <tahjallman64@gmail.com>
2024-06-17 23:31:58 -0400

    still trying to figure out the equal height issue of the textareas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     const handleInput = (textArea: HTMLTextAreaElement) => {
       textArea.style.height = "auto";
       const scrollHeight = textArea.scrollHeight;
-      textArea.style.height = scrollHeight + "px";
       textArea.style.height = `${Math.min(scrollHeight, 300)}px`;
     };
 
@@ -25,8 +24,9 @@ export default function Home() {
       // Initial resize to fit the initial content
       handleJobInput();
       cleanupFunctions.push(() => {
+        console.log("cleanup job");
         textAreaJob.removeEventListener("input", handleJobInput);
-    });
+      });
     }
 
     if (textAreaResume) {
@@ -35,20 +35,21 @@ export default function Home() {
       // Initial resize to fit the initial content
       handleResumeInput();
       cleanupFunctions.push(() => {
+        console.log("cleanup resume");
         textAreaResume.removeEventListener("input", handleResumeInput);
-    });
+      });
     }
 
     return () => {
       cleanupFunctions.forEach((cleanup) => cleanup());
     };
-  }, []);
-  
+  }, [textAreaJobDescription, textAreaResumeContent]);
+
   return (
     <main className="flex flex-col min-h-screen justify-start items-center p-5">
       <h1 className="text-4xl mb-3">CVMasterpiece</h1>
       <h4 className="mb-6">Create your perfect CV/Resume</h4>
-      <div className="w-full grid grid-cols-2 gap-4 auto-cols-min">
+      <div className="w-full grid grid-cols-2 gap-4 auto-rows-min">
         <textarea
           ref={textAreaJobDescription}
           name="job-description-textarea"
